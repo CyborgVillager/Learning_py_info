7e8c02d635a9104921297b31b4f9d0ada71ba0ee
CyborgVillager <almawijonathan@gmail.com>
2019-12-18 10:07:52 -0600

    v1 - update

--- varia.py
+++ varia.py
@@ -1,9 +1,18 @@
 #variables
-name = "John"
-age = 25
-para0 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum, odio et feugiat auctor, ante leo t" \
-        "incidunt tortor, sed lacinia leo augue vel lorem. In rutrum est libero"
-para1 = "Nunc euismod magna in diam finibus sollicitudin. Aliquam commodo tortor lorem, in tincidunt quam dapibus " \
-        "fringilla. Duis vitae sem ut ligula efficitur varius."
 
-print(name, 'is age', str(age),  para0, '\n', para1)
+def story_example():
+        name = "John"
+        age = 25
+        para0 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum, odio et feugiat auctor, ante leo t" \
+                "incidunt tortor, sed lacinia leo augue vel lorem. In rutrum est libero"
+        para1 = "Nunc euismod magna in diam finibus sollicitudin. Aliquam commodo tortor lorem, in tincidunt quam dapibus " \
+                "fringilla. Duis vitae sem ut ligula efficitur varius."
+
+        print(name, 'is age', str(age),  para0, '\n', para1)
+
+
+def story_start():
+        story_example()
+
+
+story_start()
