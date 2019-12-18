ee426b45cc88f9fbbc9f82f3304ca7d07a3ed9d8
CyborgVillager <almawijonathan@gmail.com>
2019-12-18 09:33:27 -0600

    v0
    re-intro / summary of py. after class

---
+++ triangle.py
@@ -0,0 +1,13 @@
+
+def right_angle_side():
+    print("    /|")
+    print("   / |")
+    print("  /  |")
+    print(" /   |")
+    print("/____|")
+
+
+def main():
+    right_angle_side()
+
+main()
