7f739203dfe2fe654efbd4cfeaa33f9c71bab377
CyborgVillager <almawijonathan@gmail.com>
2019-12-19 10:59:57 -0600

    v9

---
+++ basic info/tuple/tuple.py
@@ -0,0 +1,4 @@
+coordinates = ( 4, 5)
+# coordinates = [ 4, 5] -> you can modify since its a list tuple uses ()
+# this will be an error since its a tuple - good for coord, or stable info coordinates[1] = 55
+print(coordinates[0])
