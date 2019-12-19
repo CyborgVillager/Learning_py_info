fa3c826a7fc6ca7b22cbebfea79b45722113667a
CyborgVillager <almawijonathan@gmail.com>
2019-12-19 09:10:57 -0600

    v6

---
+++ mad_lib_gme/mad_lib_game_basic.py
@@ -0,0 +1,23 @@
+def mad_lib_no_user_input_1st():
+    print('A.I is {plural_noun} ')
+    print('The Sky is  {color} ')
+    print('You have to write a color & a noun to explain the story')
+
+
+def mad_lib_story_explained():
+    plural_noun = input("Enter a Plural Noun: ")
+    color = input("Enter a color: ")
+
+    print('A.I is ' + plural_noun + '!')
+    print('The Sky is ' + color)
+
+
+def space():
+    print('--------Respond Accordinaly-------')
+
+def main():
+    mad_lib_no_user_input_1st()
+    space()
+    mad_lib_story_explained()
+
+main()
