6130014bf6e94822f55d0168931457c99122d575
CyborgVillager <almawijonathan@gmail.com>
2019-12-19 09:21:07 -0600

    v7

---
+++ basic info/lists/list0.py
@@ -0,0 +1,19 @@
+#basic list
+
+def basic_list():
+    user_names = ['Lorem', 'lo', 'Vinny','kria']
+    user_names[1] = 'RT'
+    age = [15 , 17 , 18 ,16]
+
+    print(user_names[2])
+    print(user_names[-2])
+    print(user_names[1:3])
+    print(age[1])
+
+def main0():
+    basic_list()
+
+
+main0()
+
+
