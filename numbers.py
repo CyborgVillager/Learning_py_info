a07fbe2f81edce6d62ac5da2d553258051364149
CyborgVillager <almawijonathan@gmail.com>
2019-12-19 08:52:37 -0600

    v4

---
+++ numbers.py
@@ -0,0 +1,14 @@
+from math import *
+
+abs_num = -5
+print(abs(abs_num))
+print(pow(3, 2)) # 3^2
+print(max(6, 8))
+print(min(6, 8))
+
+# import math info start
+#round the numb down
+print(floor(3.7))
+#round the numb up
+print(ceil(3.7))
+print(sqrt(36))
--- strings.py
+++ strings.py
@@ -1,3 +1,9 @@
 internet_name =("CyborgVillager & \n Jonathan A.i")
 sentence = (" are constantly growing in knowledge & abilities ")
 print(internet_name + sentence[5:30])
+# index -> help find where the info is can be used for list & other exts.
+print(internet_name.index("Villager"))
+print(internet_name.index("Jo"))
+print(internet_name.index("&"))
+# replace a word/info with a new one
+print(internet_name.replace("Jonathan", "John"))
