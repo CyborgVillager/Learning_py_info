9804d4b293c5911b0f1a2054fb9546ffbbdb4132
CyborgVillager <almawijonathan@gmail.com>
2020-01-05 08:06:03 -0600

    v6 tidy up

--- in_depth_info/card_game/main_card.py
+++ in_depth_info/card_game/main_card.py
@@ -11,13 +11,15 @@ print('You have draw a :', test_the_card.card_value,test_the_card.card_suit)
 player0_card = []
 player1_card = []
 
+
 # Deal x amount of players for deal of war gameplay
 def deal_war():
-    while(len(partial_deck_drawn) > 0):
+    while (len(partial_deck_drawn) > 0):
         player0_card.append(draw_cards(partial_deck_drawn))
         player1_card.append(draw_cards(partial_deck_drawn))
-deal_war()
 
+
+deal_war()
 
 # Now to let the game compare each player's card
 # & check for info if players have = scores.
@@ -25,34 +27,42 @@ deal_war()
 count_player0 = 0
 count_player1 = 0
 
+
 def space():
     print('\n')
+
+
 def line_space():
     print('------- ------- ------- -------')
 
+
 # Back end info from card_game.py in regarding to card_value
 # Player text colors & score color can be found in source under card_game -> text_color.py
-for index in range(0,len(player0_card)):
+for index in range(0, len(player0_card)):
     if player0_card[index].card_value > player1_card[index].card_value:
         count_player0 += 1
-        print(player0_color +'Player 0' + end + win_score + ' wins' + end +' the hand with: ' + card_color , player0_card[index].card_value)
-        print(player1_color +'Player 1' + end + lose_score + ' loses' + end + ' with card: ' + card_color , player1_card[index].card_value)
-        print(player0_color +'Player 0' + end + total_score +' Total Score: ' + end + str(count_player0))
+        print(player0_color + 'Player 0' + end + win_score + ' wins' + end + ' the hand with: ' + card_color,
+              player0_card[index].card_value)
+        print(player1_color + 'Player 1' + end + lose_score + ' loses' + end + ' with card: ' + card_color,
+              player1_card[index].card_value)
+        print(player0_color + 'Player 0' + end + total_score + ' Total Score: ' + end + str(count_player0))
         space()
         line_space()
     if player0_card[index].card_value < player1_card[index].card_value:
         count_player1 += 1
-        print(player1_color + 'Player 1' + end + win_score + ' wins' + end + ' the hand with: ' + card_color , player1_card[index].card_value)
-        print(player0_color +'Player 0 ' + end +  lose_score + 'loses' + end + ' with card: ' + card_color , player0_card[index].card_value)
+        print(player1_color + 'Player 1' + end + win_score + ' wins' + end + ' the hand with: ' + card_color,
+              player1_card[index].card_value)
+        print(player0_color + 'Player 0 ' + end + lose_score + 'loses' + end + ' with card: ' + card_color,
+              player0_card[index].card_value)
         print(player1_color + 'Player 1 ' + end + total_score + 'Total Score: ' + end + str(count_player1))
         space()
         line_space()
     else:
-        print(war_color +'War! Both Player\'s take losses!!' + end)
+        print(war_color + 'War! Both Player\'s take losses!!' + end)
         count_player0 -= 1
         count_player1 -= 1
         space()
-        print(player0_color +'Player0\'s Score: ' + total_score + str(count_player0))
+        print(player0_color + 'Player0\'s Score: ' + total_score + str(count_player0))
         print(player1_color + 'Player1\'s Score: ' + total_score + str(count_player1))
         space()
 
@@ -60,20 +70,19 @@ for index in range(0,len(player0_card)):
     player_0_win_margin = count_player0 - count_player1
     player_1_win_margin = count_player1 - count_player0
 
-
     # Player 0 Victory
     if count_player0 > count_player1:
-        print(player0_color +'Player0\'s wins by a margin of ' + war_player_score + str(player_0_win_margin))
-        print(player0_color +'Player0\'s orignal score was: ' + war_player_score +  str(count_player0))
+        print(player0_color + 'Player0\'s wins by a margin of ' + war_player_score + str(player_0_win_margin))
+        print(player0_color + 'Player0\'s orignal score was: ' + war_player_score + str(count_player0))
         print(player1_color + 'Player1\'s orignal score was: ' + war_player_score + str(count_player1))
         print(winner_text + '---------- ---------- ---------- WINNER' + player0_color + ' Player0 '
-                          + end + winner_text + '---------- ---------- ----------')
+              + end + winner_text + '---------- ---------- ----------')
     # Player 1 Victory
     elif count_player1 > count_player0:
-        print(player1_color +'Player\'s 1 wins by a margin of ' + war_player_score +  str(player_1_win_margin))
-        print(player1_color +'Player1\'s orignal score was: ' + war_player_score + str(count_player1))
-        print(player0_color +'Player0\'s orignal score was: ' + war_player_score + str(count_player0))
-        print(winner_text +'---------- ---------- ---------- WINNER' + player1_color + ' Player1 '
+        print(player1_color + 'Player\'s 1 wins by a margin of ' + war_player_score + str(player_1_win_margin))
+        print(player1_color + 'Player1\'s orignal score was: ' + war_player_score + str(count_player1))
+        print(player0_color + 'Player0\'s orignal score was: ' + war_player_score + str(count_player0))
+        print(winner_text + '---------- ---------- ---------- WINNER' + player1_color + ' Player1 '
               + end + winner_text + '---------- ---------- ----------')
     # Game over if both players have = score
     else:
@@ -81,3 +90,5 @@ for index in range(0,len(player0_card)):
         print(player0_color + 'Player0 Score: ' + war_player_score + str(count_player0))
         print(player1_color + 'Player1 Score: ' + war_player_score + str(count_player1))
         print(game_over + '---------- ---------- ---------- GAME OVER- NOBODY WINS ---------- ---------- ----------')
+        space()
+    
