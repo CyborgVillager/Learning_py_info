a4c79f24fd315d16b5e5222253d085033ae0d4a0
CyborgVillager <almawijonathan@gmail.com>
2020-01-05 07:03:51 -0600

    Source Folder v4
    made the meat of the code into a source, while making the code for the player to start with be the main code.
    
    Bit more organized on the backend / front end for the code.
    
    Backend will have the random / handling on how the cards being delt with. While the front end will be for player code

---
+++ in_depth_info/card_game/main_card.py
@@ -0,0 +1,8 @@
+from card_game import *
+
+'''''
+Game will start if the user/player activated the game through main_cards.py
+
+test_the_card = draw_cards(partial_deck_drawn)
+print('You have draw a :', test_the_card.card_value,test_the_card.card_suit)
+'''''
